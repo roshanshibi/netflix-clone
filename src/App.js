3312995017776795987c62d53ab788cb139e93a2
Roshan Shibi <roshanshibi2015@gmail.com>
2022-06-22 21:34:00 +0530

    added banner and rowpost

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
+import Banner from './Components/Banner/Banner';
 import NavBar from './Components/NavBar/NavBar';
+import RowPost from './Components/RowPost/RowPost';
 
 function App() {
   return (
   <div>
     <NavBar/>
+    <Banner/>
+    <RowPost/>
   </div>
   );
 }
